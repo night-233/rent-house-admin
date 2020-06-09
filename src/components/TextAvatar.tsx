ba16b3d73245b75f880bdaa67ac5d0dedb9286c3
吴小雪 <xiaoxue.wu@upai.com>
2020-06-09 09:59:28 +0800

    calc avatar fontsize

--- src/components/TextAvatar.tsx
+++ src/components/TextAvatar.tsx
@@ -3,12 +3,23 @@ import React from 'react';
 import Tools from '@utils/tools'
 import styled from 'styled-components'
 export default ({ name, avatar, width = "32" }) => {
-  const config = Tools.calcUserIcon(name)
+  const calcFontSize = () => {
+    let widthNum = Number(width);
+    if (!isNaN(widthNum) || widthNum <= 0) {
+      const coefficient = name.length > 1 ? 0.4 : 0.5;
+      return `${Math.round(widthNum * coefficient)}px`;
+    } else {
+      return '16px';
+    }
+  }
+
   const style = {
     width: `${width}px`,
     height: `${width}px`,
-    lineHeight: `${width}px`
+    lineHeight: `${width}px`,
+    fontSize: calcFontSize()
   }
+  const config = Tools.calcUserIcon(name)
   return (
     <Avatar color={config.color} style={style}>
       {avatar ? <img src={avatar} style={style} /> : config.name}
